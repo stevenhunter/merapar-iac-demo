de09f3eba23c0167b60e757aa814dbb93f739049
Steven Hunter <stevenxhunter@gmail.com>
2022-12-05 20:06:11 +0000

    updated update-dynamic-content.sh

--- scripts/update-dynamic-content.sh
+++ scripts/update-dynamic-content.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+clear
 read -p "Enter new value for dynamic string: " dynamicString
+echo
 read -p "This will update the value stored to "\""$dynamicString"\""? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-
 aws dynamodb update-item \
     --table-name merapar-iac-storage \
     --key '{"key1" : {"S": "dynamic-content"}}' \
